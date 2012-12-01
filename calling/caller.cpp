9b45f434bc6a072ef14ec544f8f16920f7768649
Nigel Atkinson <merlinblack@users.noreply.github.com>
2012-12-01 21:46:35 +1100

    More experimentation

---
+++ calling/caller.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <lua.hpp>
+#include <LuaBridge.h>
+
+using std::cout;
+using std::endl;
+using luabridge::LuaRef;
+using luabridge::getGlobal;
+
+void test( lua_State* L )
+{
+    cout << "Stack top: " << lua_gettop( L ) << endl;
+
+    const char *source =
+        "function test( a, b, c )\n"
+        "    print( a, b, c )\n"
+        "    return a, b, c\n"
+        "end\n"
+        "";
+
+    if( luaL_dostring( L, source ) )
+    {
+        cout << lua_tostring( L, -1 );
+        lua_pop( L, 1 );
+        return;
+    }
+
+    cout << "Stack top: " << lua_gettop( L ) << endl;
+
+    LuaRef test = getGlobal( L, "test" );
+
+    cout << "Stack top: " << lua_gettop( L ) << endl;
+
+    test( 1, 2, 3 );
+
+    cout << "Stack top: " << lua_gettop( L ) << endl;
+
+    return;
+}
+
+int main()
+{
+    lua_State* L = luaL_newstate();
+
+    luaL_openlibs( L );
+
+    test( L );
+
+    return EXIT_SUCCESS;
+}
