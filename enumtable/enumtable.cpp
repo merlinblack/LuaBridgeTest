4838d0c32e6036f7449746c0bf174a8b8a53b07e
Nigel Atkinson <nigel@atkinson.gen.nz>
2013-06-24 10:45:48 +1000

    Readme and enumtable readonly / comments

--- enumtable/enumtable.cpp
+++ enumtable/enumtable.cpp
@@ -7,6 +7,10 @@ using std::cout;
 using std::endl;
 using namespace luabridge;
 
+int nopeReadOnly( lua_State* L )
+{
+    return luaL_error( L,  "Attempt to modify readonly table.  You scoundrel." );
+}
 void test( lua_State* L )
 {
     getGlobalNamespace( L )
@@ -19,15 +23,35 @@ void test( lua_State* L )
     enums["B"] = 2;
     enums["C"] = 3;
 
+    // Simple write protecting - has some flaws
+    LuaRef metatable = newTable( L );
+    metatable["__index"] = enums;
+    metatable["__newindex"] = nopeReadOnly;
+    metatable["__metatable"] = false;
+    LuaRef readonlyEnums = newTable(L);
+    LuaRef setmetatable = getGlobal( L,  "setmetatable" );
+    setmetatable( readonlyEnums, metatable );
+    //
+    // This works to make the table read only unless a script
+    // uses table.insert, or rawset.  Also nested tables are not
+    // protected. There are ways of doing this - see lua-users.org
+    // A __pairs metamethod may be needed too.
+
     LuaRef MyClasses = getGlobal( L, "MyClasses" );
 
-    MyClasses["Enum"] = enums;
+    // You can't do this in Lua - the table is protected via metatable.
+    MyClasses["Enum"] = readonlyEnums;
 
     const char *source =
         "function test()\n"
+        "  print( 'test()' )\n"
         "  print( MyClasses.Enum.A )\n"
         "  print( MyClasses.Enum.B )\n"
         "  print( MyClasses.Enum.C )\n"
+        "end\n"
+        "function test2()\n"
+        "  print( 'test2()' )\n"
+        "  MyClasses.Enum.A = 5\n"
         "end\n"
         "";
 
@@ -40,9 +64,20 @@ void test( lua_State* L )
         return;
     }
 
-    LuaRef test = getGlobal( L, "test" );
+    try
+    {
+        LuaRef test = getGlobal( L, "test" );
 
-    test();
+        test();
+
+        LuaRef test2 = getGlobal( L, "test2" );
+
+        test2(); // Should throw exception
+    }
+    catch( std::exception &e )
+    {
+        cout << e.what() << endl;
+    }
 
     return;
 }
